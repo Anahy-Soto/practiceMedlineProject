ce0cfdf6892b4622543a101d88be750ecfb6dc3b
jrivera1217 <144291459+jrivera1217@users.noreply.github.com>
2023-10-27 11:46:26 -0500

    Update Homepage.js

--- src/Homepage.js
+++ src/Homepage.js
@@ -10,4 +10,19 @@ function Homepage() {
     );
 }
 
+export default Homepagimport React from 'react';import Navbar from './Components/Navbar';
+import Sidebar from './Components/sidebar';
+import Toggle from './Components/scripts';
+
+function Homepage() {
+    return (
+        <div className="Homepage">
+            <Navbar />
+            <Sidebar />
+            <Toggle />
+        </div>
+    );
+}
+
 export default Homepage;
+e;
