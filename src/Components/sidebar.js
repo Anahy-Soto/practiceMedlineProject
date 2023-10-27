12531e7fb65a6b274b91342899947afc8bee6a8d
jrivera1217 <144291459+jrivera1217@users.noreply.github.com>
2023-10-27 11:43:48 -0500

    Update sidebar.js

--- src/Components/sidebar.js
+++ src/Components/sidebar.js
@@ -1,46 +1,49 @@
-import React from 'react';
-import styles from '../Sidebar.module.css';
+import React from "react";
+import '../styles.css';
 
 function Sidebar() {
-    return (
-        <div className={styles.sidebar}>
-            <div className={styles.logo}>
-                <i className="bx bx-menu menu-icon"></i>  
-                <span className={styles.logoName}>Medline</span>
-            </div>
-            <div className={styles.sidebarContent}>
-                <ul className={styles.lists}>
-                    <li className={styles.list}>
-                        <a href="/index.html" className={styles.navLink}>
-                            <i className="bx bx-home-alt icon"></i> 
-                            <span className={styles.link}>Dashboard</span>
-                        </a>
-                    </li>
-                    <li className={styles.list}>
-                        <a href="/Archives.html" className={styles.navLink}>
-                            <i className="bx bx-box icon"></i>  
-                            <span className={styles.link}>Archives</span>
-                        </a>
-                    </li>
-                    // ... [other similar items]
-                </ul>
-                <div className={styles.bottomContent}>
-                    <li className={styles.list}>
-                        <a href="#" className={`${styles.navLink} ${styles.settings}`}>
-                            <i className="bx bx-cog icon"></i>  
-                            <span className={styles.link}>Settings</span>
-                        </a>
-                    </li>
-                    <li className={styles.list}>
-                        <a href="#" className={`${styles.navLink} ${styles.logout}`}>
-                            <i className="bx bx-log-out icon"></i>  
-                            <span className={styles.link}>Logout</span>
-                        </a>
-                    </li>
-                </div>
+  return (
+        <div className="sidebar">
+        <link
+            href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+            rel="stylesheet"
+        />
+        <div className="logo">
+            <i className="bx bx-menu menu-icon"></i>
+            <span className="logo-name">Medline</span>
+        </div>
+        <div className="sidebar-content">
+            <ul className="lists">
+            <li className="list">
+                <a href="/index.html" className="nav-link">
+                <i className="bx bx-home-alt icon"></i>
+                <span className="link">Dashboard</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="/Archives.html" className="nav-link">
+                    <i class="bx bx-box icon"></i>
+                    <span className="link">Archives</span>
+                </a>
+                </li>
+            </ul>
+            <div className="lists">
+            <li className="list">
+                <a href="/" className="nav-link settings">
+                <i className="bx bx-cog icon"></i>
+                <span className="link">Settings</span>
+                </a>
+            </li>
+            <li className="list">
+                <a href="/" className="nav-link logout">
+                <i className="bx bx-log-out icon"></i>
+                <span className="link">Logout</span>
+                </a>
+            </li>
             </div>
         </div>
-    );
-}
-
+        <section className="overlay"></section>;
+        </div>
+  );
+};
 export default Sidebar;
