a0d3df00b8cb0ed36f8e142f4a5db9aa3d271f99
jrivera1217 <144291459+jrivera1217@users.noreply.github.com>
2023-10-30 19:32:20 -0500

    Update Homepage.js

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,28 +1,19 @@
-import React from 'react';import Navbar from './Components/Navbar';
-import Sidebar from './Components/sidebar';
-
-function Homepage() {
-    return (
-        <div className="Homepage">
-            <Navbar />
-            <Sidebar />
-        </div>
-    );
-}
-
-export default Homepagimport React from 'react';import Navbar from './Components/Navbar';
-import Sidebar from './Components/sidebar';
+import React from 'react';
+import Navbar from './Components/Navbar';
+// import Sidebar from './Components/sidebar';
 import Toggle from './Components/scripts';
+import Temps from './Components/Temps';
+import Recents from './Components/Recents';
 
 function Homepage() {
     return (
         <div className="Homepage">
-            <Navbar />
-            <Sidebar />
             <Toggle />
+            <Temps />
+            <Recents />
         </div>
     );
 }
 
 export default Homepage;
-e;
+
