52fd44db29ece18c98a5ff7a20abfb8972559933
jrivera1217 <144291459+jrivera1217@users.noreply.github.com>
2023-10-30 21:08:17 -0500

    Add files via upload

---
+++ src/Archives.js
@@ -0,0 +1,161 @@
+import React from 'react';
+import Toggle from './Components/scripts';
+
+function Archives() {
+  return (
+    <div>
+        <Toggle />
+        <h1 class="top">Archives</h1>
+    <div class="archives-top">
+    <div class="archives-content">
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard1</p>
+          </div>
+          <div class="col-1 item-2 items">10/18/2023</div>
+        </div>
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard2</p>
+          </div>
+          <div class="col-1 item-2">10/10/2023</div>
+        </div>
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard3</p>
+          </div>
+          <div class="col-1 item-2">9/26/2023</div>
+        </div>
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard4</p>
+          </div>
+          <div class="col-1 item-2">9/16/2023</div>
+        </div>
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard5</p>
+          </div>
+          <div class="col-1 item-2">9/01/2023</div>
+        </div>
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard6</p>
+          </div>
+          <div class="col-1 item-2">8/29/2023</div>
+        </div>
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard1</p>
+          </div>
+          <div class="col-1 item-2 items">10/18/2023</div>
+        </div>
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard2</p>
+          </div>
+          <div class="col-1 item-2">10/10/2023</div>
+        </div>
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard3</p>
+          </div>
+          <div class="col-1 item-2">9/26/2023</div>
+        </div>
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard4</p>
+          </div>
+          <div class="col-1 item-2">9/16/2023</div>
+        </div>
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard5</p>
+          </div>
+          <div class="col-1 item-2">9/01/2023</div>
+        </div>
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard6</p>
+          </div>
+          <div class="col-1 item-2">8/29/2023</div>
+        </div>
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard1</p>
+          </div>
+          <div class="col-1 item-2 items">10/18/2023</div>
+        </div>
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard2</p>
+          </div>
+          <div class="col-1 item-2">10/10/2023</div>
+        </div>
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard3</p>
+          </div>
+          <div class="col-1 item-2">9/26/2023</div>
+        </div>
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard4</p>
+          </div>
+          <div class="col-1 item-2">9/16/2023</div>
+        </div>
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard5</p>
+          </div>
+          <div class="col-1 item-2">9/01/2023</div>
+        </div>
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard6</p>
+          </div>
+          <div class="col-1 item-2">8/29/2023</div>
+        </div>
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard1</p>
+          </div>
+          <div class="col-1 item-2 items">10/18/2023</div>
+        </div>
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard2</p>
+          </div>
+          <div class="col-1 item-2">10/10/2023</div>
+        </div>
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard3</p>
+          </div>
+          <div class="col-1 item-2">9/26/2023</div>
+        </div>
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard4</p>
+          </div>
+          <div class="col-1 item-2">9/16/2023</div>
+        </div>
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard5</p>
+          </div>
+          <div class="col-1 item-2">9/01/2023</div>
+        </div>
+        <div class="archives-items">
+          <div class="col-1 item-1">
+            <p>Scorecard6</p>
+          </div>
+          <div class="col-1 item-2">8/29/2023</div>
+        </div>
+    </div>
+    </div>
+    </div>
+  )
+};
+
+export default Archives;
