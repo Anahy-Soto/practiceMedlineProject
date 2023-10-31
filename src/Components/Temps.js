663d64b0c7cd5a5c1e04012561103bd607a5a8b7
jrivera1217 <144291459+jrivera1217@users.noreply.github.com>
2023-10-30 19:33:30 -0500

    Add files via upload

---
+++ src/Components/Temps.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function Temps() {
+  return (
+    <div className='temps'>
+        <div id="dash" class="create-section">
+      <ul class="lists-create">
+        <li class="templates create-item">
+          <div class="blank-page-item"></div>
+          <span class="labels blank-page-label">Blank</span>
+        </li>
+        <li class="templates create-item">
+          <div class="blank-page-item"></div>
+          <span class="labels blank-page-label">temp</span>
+        </li>
+        <li class="templates create-item">
+          <div class="blank-page-item"></div>
+          <span class="labels blank-page-label">temp</span>
+        </li>
+        <li class="templates create-item">
+          <div class="blank-page-item"></div>
+          <span class="labels blank-page-label">temp</span>
+        </li>
+        <li class="more-btn create-item">
+          <i class='bx bx-dots-vertical-rounded more-icon'></i>
+          <span class="labels more-label">more</span>
+        </li>
+      </ul>
+    </div>
+    </div>
+  );
+}
+
+export default Temps
