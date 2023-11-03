6a958295ed8f4b85c5c4dee26ecff20ad118ed03
jrivera1217 <144291459+jrivera1217@users.noreply.github.com>
2023-11-03 14:59:42 -0500

    Update App.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,21 +1,61 @@
-import { Route, Routes } from "react-router-dom";
-import React from "react";
-import Homepage from "./pages/Homepage";
-import Login from "./Components/Login";
-import Archives from "./pages/Archives";
-import Creation from "./pages/creation";
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import Toggle from './Components/scripts';
+import SingleScorecard from './pages/SingleScorecard';
+import Archives from './pages/Archives';
+import Login from './Components/Login';
+import Homepage from './pages/Homepage';
+import Creation from './pages/creation';
+
+const Layout = () => {
+  return (
+    <>
+      <Toggle />
+      <Outlet />
+    </>
+  )
+}
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/homepage",
+        element:<Homepage />
+      },
+      {
+        path:"/singleScorecard/:id",
+        element:<SingleScorecard />
+      },
+      {
+        path:"/Archives",
+        element:<Archives />
+      },
+      {
+        path:"/Create",
+        element:<Creation />
+      },
+
+
+    ]
+  },
+  {
+    path:"/login",
+    element:<Login />
+  },
+])
+
 
 function App() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/Homepage" element={<Homepage />} />
-        <Route path="/Archives" element={<Archives />} />
-        <Route path="/Create" element={<Creation />} />
-      </Routes>
+  return <div className='app'>
+    <div className='container'>
+    <RouterProvider router={router} />
     </div>
-  );
+    </div>;
 }
+
+
 
 export default App;
