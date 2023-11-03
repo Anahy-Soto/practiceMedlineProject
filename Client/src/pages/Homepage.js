0aab99a15293534a354278d14ac9744f8b368f5e
jrivera1217 <144291459+jrivera1217@users.noreply.github.com>
2023-11-03 14:56:38 -0500

    Update Homepage.js

--- Client/src/pages/Homepage.js
+++ Client/src/pages/Homepage.js
@@ -1,13 +1,10 @@
 import React from "react";
-// import Sidebar from './Components/sidebar';
-import Toggle from "../Components/scripts";
 import Temps from "../Components/Temps";
 import Recents from "../Components/Recents";
 
 function Homepage() {
   return (
     <div className="Homepage">
-      <Toggle />
       <Temps />
       <Recents />
     </div>
