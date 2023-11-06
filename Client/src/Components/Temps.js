33ef35fce61864523437f2a0c49a78afaba2e3c1
jrivera1217 <144291459+jrivera1217@users.noreply.github.com>
2023-11-06 10:48:16 -0600

    Update Temps.js

--- Client/src/Components/Temps.js
+++ Client/src/Components/Temps.js
@@ -8,7 +8,7 @@ function Temps() {
         <div id="dash" class="create-section">
       <ul class="lists-create">
         <li class="templates create-item">
-          <Link className="labels blank-page-label link1" to={'/Create'}>
+          <Link className="labels blank-page-label link1" to={'/pages/Create'}>
             <div class="blank-page-item"></div>
             <span class="labels blank-page-label">Blank</span>
           </Link>
